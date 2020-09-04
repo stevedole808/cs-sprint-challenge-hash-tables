a01830e84e257be21a7adc25e4191bd0abdc2248
stevedole808 <stevedolelambda@gmail.com>
2020-09-04 05:53:28 -1000

    finished ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,34 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    cache = {}
+    # add the source and destination in the cache
+    for i in tickets:
+        cache[i.source] = i.destination
+    
+    route = []
+    # start from 'NONE' because that's the beggining of the trip
+    current = 'NONE'
+    # for loop to iterate through all the routes
+    for i in range(0, length):
+        # appends the first route
+        route.append(cache[current])
+        # sets current to the first route
+        current = cache[current]
     return route
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+reconstruct_trip(tickets, 10)
